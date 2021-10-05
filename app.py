9f4f8d2d015dba0eee2ff99e9a992ad632c55956
Kar Aik Lim <limkaraik@hotmail.com>
2021-10-06 00:53:31 +0800

    add comments

--- app.py
+++ app.py
@@ -10,13 +10,15 @@ app.config["DEBUG"] = True
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-
+#get data from the json file
 graph,timeHorizon, init_attacker,init_defenders,exits = parseJsonData()
+#initialise environment, defender etc...
 environment=Env(graph, timeHorizon, init_attacker, tuple(init_defenders), exits)
 game_state=environment.reset()
 Map = Maps(graph,timeHorizon,init_attacker,tuple(init_defenders),exits)
 defender=AgentEval(Map)
 
+#reset and initialise the game 
 @app.route("/api/reset", methods=["GET"])
 @cross_origin()
 def reset():
@@ -24,13 +26,16 @@ def reset():
     global defender
     global game_state
     global environment
+    #get data from the json file
     graph,timeHorizon, init_attacker,init_defenders,exits = parseJsonData()
+    #initialise environment, defender etc...
     environment=Env(graph, timeHorizon, init_attacker, tuple(init_defenders), exits)
     Map = Maps(graph,timeHorizon,init_attacker,tuple(init_defenders),exits)
     defender=AgentEval(Map)
     game_state=environment.reset()
     return flask.jsonify({'success': True})
 
+#when the player select actions to move, return a list of next defenders positions
 #/api/move?node=attacker_new_pos
 @app.route("/api/move", methods=["GET"])
 @cross_origin()
@@ -39,6 +44,7 @@ def move():
     global defender
     global environment
     try:
+        #get attacker pos from request params
         attacker_a = int(flask.request.args.get('node'))
         defender_obs, attacker_obs = game_state.obs()
         def_current_legal_action, att_current_legal_action = game_state.legal_action()
